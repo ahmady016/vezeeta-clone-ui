fb3f3aecc7b3e322912fd3cfcb809c3733cfddc9
ahmady016 <ahmady09@gmail.com>
2022-02-25 01:53:23 +0200

    setup react-query and react-toastify

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,33 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-import App from './App'
-import './index.css'
-
 import { Router } from 'react-router-dom'
 import { createBrowserHistory } from 'history'
+
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+
+import { ToastContainer } from 'react-toastify'
+
+import App from './App'
+
+// react toastify styles, global styles
+import 'react-toastify/dist/ReactToastify.css'
+import './index.css'
+
+// react router history
 export const history = createBrowserHistory()
+// react query client
+export const queryClient = new QueryClient()
 
 render(
 	<React.StrictMode>
 		<Router history={history}>
-			<App />
+			<QueryClientProvider client={queryClient}>
+				<App />
+				<ReactQueryDevtools />
+				<ToastContainer newestOnTop />
+			</QueryClientProvider>
 		</Router>
 	</React.StrictMode>,
 	document.getElementById('root')
