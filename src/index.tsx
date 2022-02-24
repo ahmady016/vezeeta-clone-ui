79ba68418aa8ed2d1c1d3235ec264bca440edddd
ahmady016 <ahmady09@gmail.com>
2022-02-25 01:45:36 +0200

    setup react-router and browser history

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,15 @@ import { render } from 'react-dom'
 import App from './App'
 import './index.css'
 
+import { Router } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
+export const history = createBrowserHistory()
+
 render(
 	<React.StrictMode>
-		<App />
+		<Router history={history}>
+			<App />
+		</Router>
 	</React.StrictMode>,
 	document.getElementById('root')
 )
